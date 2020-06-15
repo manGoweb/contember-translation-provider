c9c2ded195a945c186c11a004e7277a05c805541
Filip Chalupa <chalupa.filip@gmail.com>
2020-06-15 13:53:38 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Contember Translation Provider
+
+## Installation
+
+`npm install @mangoweb/contember-translation-provider`
---
+++ src/index.tsx
@@ -0,0 +1,129 @@
+import * as React from 'react'
+
+const ContemberTranslationContext = React.createContext({
+	// @TODO: add console.warn that these too should never be called without Provider
+	translate: (_: string): string => ' ',
+	setLanguage: (_: string) => {},
+	isLoadingInitialLanguage: true,
+	isLoadingAnotherLanguage: false,
+	isError: false,
+})
+
+const TRANSLATIONS_BASE_URL = 'https://translations-gw.mgw.cz/get'
+
+const fetchTranslations = async (
+	project: string,
+	stage: string,
+	domain: string,
+	language: string
+) => {
+	const response = await fetch(
+		`${TRANSLATIONS_BASE_URL}/${project}/${stage}/${domain}/${language}`
+	)
+	const data = await response.json()
+	if (typeof data !== 'object' || typeof data.translations !== 'object') {
+		throw new Error('Invalid translation data.')
+	}
+	return data.translations as {
+		[key: string]: string
+	}
+}
+
+export interface ContemberTranslationProviderProps {
+	project: string
+	stage: string
+	domain: string | string[]
+	initialLanguage: string
+}
+
+export const ContemberTranslationProvider: React.FunctionComponent<ContemberTranslationProviderProps> = (
+	props
+) => {
+	const [allTranslations, setAllTranslations] = React.useState<{
+		[language: string]: {
+			[key: string]: string
+		}
+	}>({})
+	const context = React.useContext(ContemberTranslationContext)
+	const [isLoadingTranslations, setIsLoadingTranslations] = React.useState(true)
+	const [isSomethingWrong, setIsSomethingWrong] = React.useState(false)
+	const [currentLanguage, setCurrentLanguage] = React.useState(
+		props.initialLanguage
+	)
+	const [lastLoadedUsedLanguage, setLastLoadedUsedLanguage] = React.useState(
+		props.initialLanguage
+	)
+
+	const onError = (error: Error) => {
+		console.error(error)
+		setIsSomethingWrong(true)
+	}
+
+	React.useEffect(() => {
+		if (typeof props.domain === 'object') {
+			onError(new Error('Multiple domains are not supported yet.'))
+		} else if (typeof allTranslations[currentLanguage] === 'undefined') {
+			setIsLoadingTranslations(true)
+			;(async () => {
+				try {
+					const translations = await fetchTranslations(
+						props.project,
+						props.stage,
+						props.domain as string,
+						currentLanguage
+					)
+					setAllTranslations({
+						...allTranslations,
+						[currentLanguage]: translations,
+					})
+					setLastLoadedUsedLanguage(currentLanguage)
+				} catch (error) {
+					onError(error)
+				}
+				setIsLoadingTranslations(false)
+			})()
+		} else {
+			setLastLoadedUsedLanguage(currentLanguage)
+		}
+	}, [currentLanguage])
+
+	const setLanguage = (language: string) => setCurrentLanguage(language)
+	const isLoadingInitialLanguage =
+		isLoadingTranslations &&
+		typeof allTranslations[props.initialLanguage] === 'undefined'
+	const isLoadingAnotherLanguage =
+		isLoadingTranslations && !isLoadingInitialLanguage
+	const isError = isSomethingWrong
+
+	const translate = (key: string) => {
+		if (isLoadingInitialLanguage) {
+			return context.translate(key)
+		}
+		return allTranslations[lastLoadedUsedLanguage][key]
+	}
+
+	return (
+		<ContemberTranslationContext.Provider
+			value={{
+				translate,
+				setLanguage,
+				isLoadingInitialLanguage,
+				isLoadingAnotherLanguage,
+				isError,
+			}}
+		>
+			{props.children}
+		</ContemberTranslationContext.Provider>
+	)
+}
+
+export const useTranslate = () =>
+	React.useContext(ContemberTranslationContext).translate
+export const useSetLanguage = () =>
+	React.useContext(ContemberTranslationContext).setLanguage
+export const useIsLoadingInitialLanguage = () =>
+	React.useContext(ContemberTranslationContext).isLoadingInitialLanguage
+export const useIsLoadingAnotherLanguage = () =>
+	React.useContext(ContemberTranslationContext).isLoadingAnotherLanguage
+export const useIsError = () =>
+	React.useContext(ContemberTranslationContext).isError
